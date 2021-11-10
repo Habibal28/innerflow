47f20d326ab479481a36f11d11cc7c09d3012ea6
Habibal28 <emzob30@gmail.com>
2021-11-11 00:46:12 +0700

    sidebar,home admin

--- application/views/administrator/index.php
+++ application/views/administrator/index.php
@@ -7,20 +7,66 @@
             <div class="section-header-breadcrumb">
               <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
               <div class="breadcrumb-item"><a href="#">Google Maps</a></div>
-              <div class="breadcrumb-item">Simple Map</div>
             </div>
           </div>
-
-          <div class="section-body">
-            <div class="row">
+          <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+              <div class="card card-statistic-1">
+                <div class="card-icon bg-primary">
+                  <i class="far fa-user"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Members</h4>
+                  </div>
+                  <div class="card-body">
+                    10
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+              <div class="card card-statistic-1">
+                <div class="card-icon bg-danger">
+                  <i class="far fa-newspaper"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Events</h4>
+                  </div>
+                  <div class="card-body">
+                    42
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6 col-12">
+              <div class="card card-statistic-1">
+                <div class="card-icon bg-warning">
+                  <i class="far fa-file"></i>
+                </div>
+                <div class="card-wrap">
+                  <div class="card-header">
+                    <h4>Materi</h4>
+                  </div>
+                  <div class="card-body">
+                    6
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- main -->
+          <div class="section-body" >
+            <div class="row" >
               <div class="col-12">
                 <div class="card">
                   <div class="card-header">
-                    <h4>Map</h4>
+                    <h4></h4>
                   </div>
                   <div class="card-body">
-                    <div id="simple-map" data-height="400">
-                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error delectus officia optio cupiditate eos atque odit aspernatur odio ea officiis inventore, quibusdam tenetur architecto aut corporis cumque, in omnis quis.</p>
+                    <div id="simple-map" data-height="300">
+        
                     </div>
                   </div>
                 </div>
--- application/views/templates/dashboard_sidebar.php
+++ application/views/templates/dashboard_sidebar.php
@@ -4,26 +4,24 @@
             <a href="index.html">Innerflow</a>
           </div>
           <div class="sidebar-brand sidebar-brand-sm">
-            <a href="index.html">St</a>
+            <a href="index.html">INR</a>
           </div>
           <ul class="sidebar-menu">
               <li class="menu-header">Dashboard</li>
+              <a href="#" class="nav-link has-dropdown"><i class="fas fa-fw fa-fire"></i><span class="ml-2">Home</span></a>
+              <li class="menu-header">Administrator</li>
               <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+                <a href="#" class="nav-link has-dropdown text-primary" data-toggle="dropdown"><i class="fas fa-fw fa-columns "></i> <span>Management</span></a>
                 <ul class="dropdown-menu">
-                  <li><a class="nav-link" href="index-0.html">General Dashboard</a></li>
-                  <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
+                  <li><a class="nav-link" href="layout-default.html">Management User</a></li>
+                  <li><a class="nav-link" href="layout-transparent.html">Management Event</a></li>
+                  <li><a class="nav-link" href="layout-top-navigation.html">Management Materi</a></li>
                 </ul>
               </li>
               <li class="menu-header">Member</li>
-              <li class="nav-item dropdown">
-                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Member</span></a>
-                <ul class="dropdown-menu">
-                  <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
-                  <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
-                  <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
-                </ul>
-              </li>
-              <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+              <a href="#" class="nav-link "><i class="fas fa-fw fa-user"></i><span class="ml-1">Profile</span></a>
+              <a href="#" class="nav-link"><i class="fas fa-fw fa-bullhorn"></i><span class="ml-1">Event</span></a>
+              <a href="#" class="nav-link"><i class="fas fa-fw fa-folder"></i><span class="ml-1">Materi</span></a>
+              <li><a class="nav-link mt-2 text-danger" href="blank.html"><i class="fa fa-times"></i> <span>Logout</span></a></li>
         </aside>
       </div>
